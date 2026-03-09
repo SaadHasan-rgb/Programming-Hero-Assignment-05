ded085c4c3c35e42901cab189b4503d829310986
SaadHasan-rgb <saadh4435@gmail.com>
2026-03-09 21:56:05 +0600

    selected elements added spinner function

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+// selecting elements
+const btnAll = document.getElementById("tabAllIssues");
+const btnOpen = document.getElementById("tabOpenIssues");
+const btnClosed = document.getElementById("tabClosedIssues");
+
+const gridArea = document.getElementById("issuesGrid");
+
+const totalIssuesText = document.getElementById("issueCountText");
+
+const loader = document.getElementById("loaderArea");
+
+const emptyBox = document.getElementById("emptyResult");
+
+const modal = document.getElementById("issueDetailsModal");
+const modalBody = document.getElementById("modalDataArea");
+
+const searchBtn = document.getElementById("searchActionBtn");
+const searchInput = document.getElementById("searchInputField");
+
+// storing issues globally
+let storedIssues = [];
+
+
+// showing and hiding loading spinner
+function showLoader() {
+  loader.classList.remove("hidden");
+}
+
+function hideLoader() {
+  loader.classList.add("hidden");
+}
+
+
+
